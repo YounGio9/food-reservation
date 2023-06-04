16d89d282a7f37643d8b8f6c00f818600612e824
Giovani <desouzagiovani9@gmail.com>
2023-06-04 09:22:43 +0100

    modifié :         src/components/UI/UI.scss 	modifié :         src/pages/admin/Clients.tsx

--- src/pages/admin/Clients.tsx
+++ src/pages/admin/Clients.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import clients from '../../db/clients.json'
 import { Pagination } from '@mui/material'
 import { types } from '../../helpers'
 import ClientCard from '../../components/ClientCard'
+import client from '../../helpers/client'
+import Loading from '../../components/UI/Loading'
 
 function Clients() {
    const [page, setPage] = React.useState<number>(1)
    const [totalPages, setTotalPages] = React.useState<number>(1)
    const [pageData, setPageData] = React.useState<types.Client[]>([] as types.Client[])
+   const [loading, setLoading] = React.useState<boolean>(false)
 
    const dataPerPage: number = 9
 
@@ -15,7 +17,17 @@ function Clients() {
       setPage(value)
    }
 
+   const fetchData = async () => {
+      setLoading(true)
+      const res = await client.get('/clients')
+      const fetchedData = res.data.data
+      treatData(fetchedData)
+      divideData(fetchedData)
+      setLoading(false)
+   }
+
    const treatData = (data: types.Client[]) => {
+      console.log(data)
       setTotalPages(Math.ceil(data.length / dataPerPage))
       console.log(data.length)
    }
@@ -26,38 +38,57 @@ function Clients() {
    }
 
    React.useEffect(() => {
-      treatData(clients)
-      divideData(clients)
+      fetchData()
 
       // eslint-disable-next-line
    }, [page])
 
+   // React.useEffect(() => {
+   //    getData()
+
+   // }, [page])
+
    return (
       <>
-         <div className='w-full h-full flex flex-col  px-6'>
-            <div className='flex justify-end my-4 h-8'>
-               <input
-                  name='search'
-                  className='w-1/3 rounded-full text-black p-2'
-                  placeholder='Rechercher un client'
-               />
-            </div>
+         {!loading ? (
+            <div className='w-full h-full flex flex-col  px-6'>
+               {pageData.length > 0 ? (
+                  <>
+                     <div className='flex justify-end my-4 h-8'>
+                        <input
+                           name='search'
+                           className='w-1/3 rounded-full text-black p-2'
+                           placeholder='Rechercher un client'
+                        />
+                     </div>
 
-            <div className='w-full h-auto bg-white'>
-               {pageData.map((client: types.Client, idx: number) => (
-                  <ClientCard key={(client.phoneNumber as number) + idx} client={client} />
-               ))}
+                     <div className='w-full h-auto bg-white'>
+                        {pageData.map((client: types.Client, idx: number) => (
+                           <ClientCard
+                              key={(client.phoneNumber as number) + idx}
+                              client={client}
+                           />
+                        ))}
+                     </div>
+                     <div className='flex items-center justify-center my-3'>
+                        <Pagination
+                           color='primary'
+                           page={page}
+                           count={totalPages}
+                           shape='rounded'
+                           onChange={handleChangePage}
+                        />
+                     </div>
+                  </>
+               ) : (
+                  <div className='flex items-center justify-center text-2xl mt-24'>
+                     <span>Vous n'avez pas encore de client</span>
+                  </div>
+               )}
             </div>
-            <div className='flex items-center justify-center my-3'>
-               <Pagination
-                  color='primary'
-                  page={page}
-                  count={totalPages}
-                  shape='rounded'
-                  onChange={handleChangePage}
-               />
-            </div>
-         </div>
+         ) : (
+            <Loading />
+         )}
       </>
    )
 }
--- src/pages/admin/Reservations.tsx
+++ src/pages/admin/Reservations.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import Loading from '../../components/UI/Loading'
+import { types } from '../../helpers'
+import client from '../../helpers/client'
 
 function Reservations() {
-   return <div>Reservations</div>
+   const [data, setData] = React.useState<types.Reservation[]>([])
+   const [loading, setLoading] = React.useState<boolean>(false)
+
+   const getReservations = async () => {
+      setLoading(true)
+      const data = await client.get('/reservations')
+      setData(data.data.data)
+
+      setLoading(false)
+   }
+
+   React.useMemo(() => {
+      console.log(data)
+   }, [data])
+
+   React.useEffect(() => {
+      getReservations()
+   }, [])
+
+   return loading ? <Loading /> : <div>Reservations</div>
 }
 
 export default Reservations
